66683e84b419e847ea8ac7124c1fad600e30393d
Topik Mujianto <topik.mujianto@finaccel.co>
2023-05-10 14:42:17 +0700

    Initial Commit

---
+++ TopikLib/TopikLib.h
@@ -0,0 +1,18 @@
+//
+//  TopikLib.h
+//  TopikLib
+//
+//  Created by Topik on 10/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TopikLib.
+FOUNDATION_EXPORT double TopikLibVersionNumber;
+
+//! Project version string for TopikLib.
+FOUNDATION_EXPORT const unsigned char TopikLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TopikLib/PublicHeader.h>
+
+
